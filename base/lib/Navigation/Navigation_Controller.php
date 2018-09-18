7adb251ecca6d4e697bc1ee3293bb3555a349d9a
Leano Martinet <theylooksotired@gmail.com>
2018-09-18 14:57:33 +0200

    V4.0.0

--- base/lib/Navigation/Navigation_Controller.php
+++ base/lib/Navigation/Navigation_Controller.php
@@ -152,7 +152,7 @@ class Navigation_Controller extends Controller{
 					unset($infoIns['created']);
 					unset($infoIns['modified']);
 					unset($infoIns['ord']);
-					$infoIns['ingredients2'] = array_filter((array)$infoIns['ingredients'], function($item) {return $item['label'];});
+					$infoIns['ingredients2'] = array_filter((array)$infoIns['ingredients'], function($item) {return 2;});
 					print_r($infoIns['ingredients2']);
 					$info['recipes'][] = $infoIns;
 				}
