b0bcecf9d43ed447dd86530bd67521c303921be8
Leano Martinet <theylooksotired@gmail.com>
2018-09-21 17:23:08 +0200

    V4.0.0

--- base/lib/Navigation/Navigation_Ui.php
+++ base/lib/Navigation/Navigation_Ui.php
@@ -190,7 +190,7 @@ class Navigation_Ui extends Ui {
 							<a href="https://www.cocina-ecuatoriana.com" target="_blank" title="Recetas de cocina de Ecuador">Ecuador</a>
 							<a href="https://www.la-cocina-mexicana.com" target="_blank" title="Recetas de cocina de México">México</a>
 							<a href="https://www.comida-peruana.com" target="_blank" title="Recetas de cocina del Peru">Peru</a>
-							<a href="https://www.comida-uruguaya.com" target="_blank" title="Recetas de cocina del Uruguay">Uruguay</a>
+							<a href="https://www.cocina-uruguaya.com" target="_blank" title="Recetas de cocina del Uruguay">Uruguay</a>
 						</div>
 						<div class="footerIns">
 							<div class="footerLeft">
