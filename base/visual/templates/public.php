45600d268fea0825c6350bf8cb47daa879f265c4
Leano Martinet <theylooksotired@gmail.com>
2018-10-08 11:57:13 +0200

    V4.0.0

--- base/visual/templates/public.php
+++ base/visual/templates/public.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php $version = (DEBUG) ? md5(rand()) : date('mY').'1';?>
+<?php $version = (DEBUG) ? md5(rand()) : date('mY').'2';?>
 <html lang="<?php echo Lang::active();?>">
 <head>
 
