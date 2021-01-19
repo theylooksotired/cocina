fb9f56998fa4805545e2dace906219352a4ee1f9
Raul Leano <theylooksotired@gmail.com>
2021-01-19 14:41:13 +0100

    Fixed images

--- app/lib/base/Db/Db_Object.php
+++ app/lib/base/Db/Db_Object.php
@@ -454,9 +454,22 @@ class Db_Object extends Db_Sql {
         $imageUrl = $this->getImageUrl($attributeName, $version);
         if ($imageUrl!='') {
             $imageFile = str_replace(BASE_URL, BASE_FILE, $imageUrl);
-            if (is_file($imageFile)) {
-                $imageSize = getimagesize($imageFile);
-                return '<amp-img src="'.$imageUrl.'" alt="'.$this->getBasicInfo().'" width="'.$imageSize[0].'" height="'.$imageSize[1].'" layout="intrinsic"/>';
+            $imageFileRotated = str_replace('_'.$version, '_'.$version.'_ppt', $imageFile);
+            if (!is_file($imageFileRotated)) {
+                try {
+                    $newImage = imagecreatefromjpeg($imageFile);
+                    imageflip($newImage, IMG_FLIP_HORIZONTAL);
+                    imagefilter($newImage, IMG_FILTER_COLORIZE, 35, 0, 25);
+                    imagefilter($newImage, IMG_FILTER_GAUSSIAN_BLUR, 10);
+                    $newImage = imagerotate($newImage, 3, 0);
+                    $size = min(imagesx($newImage), imagesy($newImage));
+                    $newImage = imagecrop($newImage, ['x' => $size*0.1, 'y'=>$size*0.1, 'width' => imagesx($newImage)*0.9, 'height' => imagesy($newImage)*0.8]);
+                    imagejpeg($newImage, $imageFileRotated, 70);
+                } catch (Exception $e) {}
+            }
+            if (is_file($imageFileRotated)) {
+                $imageSize = getimagesize($imageFileRotated);
+                return '<amp-img src="'.str_replace(BASE_FILE, BASE_URL, $imageFileRotated).'" alt="'.$this->getBasicInfo().'" width="'.$imageSize[0].'" height="'.$imageSize[1].'" layout="intrinsic"/>';
             }
         }
     }
@@ -467,7 +480,24 @@ class Db_Object extends Db_Sql {
     public function getImageAmpFill($attributeName, $version='', $alternative='') {
         $imageUrl = $this->getImageUrl($attributeName, $version, false);
         if ($imageUrl!='') {
-            return '<amp-img src="'.$imageUrl.'" alt="'.$this->getBasicInfo().'" layout="fill" class="amp-container"/>';
+            $imageFile = str_replace(BASE_URL, BASE_FILE, $imageUrl);
+            $imageFileRotated = str_replace('_'.$version, '_'.$version.'_ppt', $imageFile);
+            if (!is_file($imageFileRotated)) {
+                try {
+                    $newImage = imagecreatefromjpeg($imageFile);
+                    imageflip($newImage, IMG_FLIP_HORIZONTAL);
+                    imagefilter($newImage, IMG_FILTER_COLORIZE, 35, 0, 25);
+                    imagefilter($newImage, IMG_FILTER_GAUSSIAN_BLUR, 10);
+                    $newImage = imagerotate($newImage, 3, 0);
+                    $size = min(imagesx($newImage), imagesy($newImage));
+                    $newImage = imagecrop($newImage, ['x' => $size*0.1, 'y'=>$size*0.1, 'width' => imagesx($newImage)*0.9, 'height' => imagesy($newImage)*0.8]);
+                    imagejpeg($newImage, $imageFileRotated, 70);
+                } catch (Exception $e) {}
+            }
+            if ($imageFileRotated) {
+                $imageUrl = str_replace(BASE_FILE, BASE_URL, $imageFileRotated);
+                return '<amp-img src="'.$imageUrl.'" alt="'.$this->getBasicInfo().'" layout="fill" class="amp-container"/>';
+            }
         }
     }
 
